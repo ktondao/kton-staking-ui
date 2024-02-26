83fde21f832ac06aeaea678dd7f7f731063f95be
ex_suzhonghui <ex_suzhonghui@tcl.com>
2024-02-26 16:19:28 +0800

    Add sharp dependency and update webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals.filter(
+        (external) => typeof external !== 'function' || external.toString().indexOf('sharp') === -1
+      );
+    }
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
-  },
+  }
 };
 
 export default nextConfig;
