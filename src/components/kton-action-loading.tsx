c750137a9a3c15507be97fdbfa35ec630ecb9da5
ex_suzhonghui <ex_suzhonghui@tcl.com>
2024-02-27 09:47:33 +0800

    Add skeleton color variable to globals.css

--- src/components/kton-action-loading.tsx
+++ src/components/kton-action-loading.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from './ui/skeleton';
 const KTONActionLoading = () => {
   return (
     <div>
-      <Skeleton className="h-[2.5rem] w-full" />
+      <Skeleton className="h-[2.5rem] w-full " />
       <Skeleton className="mt-[0.62rem] h-[1rem] w-1/3" />
-      <Skeleton className="mt-[1.25rem] h-[2.5rem] w-full" />
+      <Skeleton className="mt-[1.25rem] h-[2.5rem] w-full " />
     </div>
   );
 };
