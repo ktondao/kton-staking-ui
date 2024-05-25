8388d55b45631bb51c97922473f08c9e736c60af
snoopy1412 <snoopy1412.su@gamil.com>
2024-05-25 22:51:38 +0800

    Implement automatic Safe login via custom hook

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import ConnectButton from '@/components/connect-button';
+import { useAppAutoConnect } from '@/hooks/useAppAutoConnect';
 
 const Header = () => {
+  useAppAutoConnect();
   return (
     <header className="h-12 w-full md:h-[3.5rem]">
       <div className="mx-auto flex h-full w-full items-center justify-between px-8">
